a5776aff42cc167e8d3f3ed6aaec7634bd0edeaa
Maxim Boykov <maeboykov@rasu.ru>
2026-04-17 14:39:12 +0300

    dev: resize vim window
    Signed-off-by: Maxim Boykov <maeboykov@rasu.ru>

--- nvim-starter/astronvim_v6/lua/custom_settings.lua
+++ nvim-starter/astronvim_v6/lua/custom_settings.lua
@@ -6,3 +6,8 @@ vim.g.editorconfig = true
 
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
+
+vim.keymap.set('n', '<M-Up>', ':resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Down>', ':resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
