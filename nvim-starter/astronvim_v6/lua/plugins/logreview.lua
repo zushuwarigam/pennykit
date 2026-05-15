005affb240033ac2f1559ad02411c1f74bae5add
bme <bme@bme.local>
2026-05-16 00:06:34 +0300

    fix: after update

--- nvim-starter/astronvim_v6/lua/plugins/logreview.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "towolf/vim-logreview",
-  cmd = "Logreview",
-  ft = "logreview",
-}
--- nvim-starter/astronvim_v6/lua/plugins/rest.lua
+++ nvim-starter/astronvim_v6/lua/plugins/rest.lua
@@ -2,6 +2,7 @@ return {
   "rest-nvim/rest.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   ft = "http",
+  build = nil,
   config = function()
     require("rest-nvim").setup()
   end,
