34fa396be27f0c0211e7e3ac35127616b28f42ce
bme <bme@bme.local>
2026-05-16 12:05:56 +0300

    fix: after update

--- nvim-starter/astronvim_v6/lua/plugins/glow.lua
+++ nvim-starter/astronvim_v6/lua/plugins/glow.lua
@@ -1,5 +1,15 @@
 return {
   "ellisonleao/glow.nvim",
   cmd = "Glow",
-  opts = {},
+  opts = {
+    -- glow_path = "",       -- Path to glow binary (auto-detected if in $PATH)
+    -- install_path = "~/.local/bin", -- Default path for installing glow binary
+    border = "shadow",    -- Floating window border style
+    style = "dark",      -- "dark" or "light" (overrides editor background)
+    pager = false,        -- Use a pager for output
+    -- width = 80,
+    -- height = 100,
+    width_ratio = 0.7,    -- Max width relative to nvim window
+    height_ratio = 0.7,   -- Max height relative to nvim window
+  },
 }
