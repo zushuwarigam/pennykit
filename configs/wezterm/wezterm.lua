6241ad1be5732659eb157232254df83ea45fcff6
Maxim Boykov <zushuwarigam@gmail.com>
2026-05-16 13:42:50 +0000

    fix: theme omb omz

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = {
 	window_decorations = "NONE",
 }
 
--- config.color_scheme = "GruvboxDark"
-config.color_scheme = "GruvboxDark"
+-- config.color_scheme = "Tokyo Night"
+config.color_scheme = "Tokyo Night"
 
 return config
