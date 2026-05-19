fad723ba4035fcbc0a8f7702f3bc365c3566bb9e
bme <bme@bme.local>
2026-05-19 21:24:55 +0300

    fix: return themes

--- nvim-starter/astronvim_v6/lua/plugins/catppuccin.lua
+++ nvim-starter/astronvim_v6/lua/plugins/catppuccin.lua
@@ -1,5 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   {
     "catppuccin/nvim",
