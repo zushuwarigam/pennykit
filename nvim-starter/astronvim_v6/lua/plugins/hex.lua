c8ab2816e354274f9696a82d1081e524875ff9c8
Maxim Boykov <maeboykov@rasu.ru>
2026-05-20 13:24:35 +0300

    fix: hex.nvim

--- nvim-starter/astronvim_v6/lua/plugins/hex.lua
+++ nvim-starter/astronvim_v6/lua/plugins/hex.lua
@@ -1,5 +1,23 @@
 return {
   "RaafatTurki/hex.nvim",
-  cmd = { "Hex", "HexAscii" },
-  opts = {},
+  cmd = { "HexDump", "HexAssemble", "HexToggle" }, -- 🔑 Lazy-load on command
+  config = function()
+    require("hex").setup({
+      dump_cmd = "xxd -g 1 -u",
+      assemble_cmd = "xxd -r",
+      is_file_binary_pre_read = function(filepath)
+        local f = io.open(filepath, "r")
+        if not f then return false end
+        local chunk = f:read(1024)
+        f:close()
+        -- Detect null bytes or high frequency of non-printable chars
+        return chunk and (string.find(chunk, "%z") ~= nil or
+          select(2, string.gsub(chunk, "[\32-\126]", "")) > 200)
+      end,
+    })
+  end,
+  -- Optional: Add keymaps
+  keys = {
+    { "<leader>H", "<cmd>HexToggle<cr>", desc = "Toggle Hex View" },
+  },
 }
