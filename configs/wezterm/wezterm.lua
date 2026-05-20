d2083026de4bf3442622dbb7889e71f23b427421
Maxim Boykov <maeboykov@rasu.ru>
2026-05-20 16:53:21 +0300

    fix: wezterm theme

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -2,9 +2,12 @@ local wezterm = require("wezterm")
 local config = {
 	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "NONE",
+  initial_cols = 120,
+  initial_rows = 28,
+  font_size = 12
 }
 
-local ok, local_theme = pcall(dofile, wezterm.config_dir .. "/_local_theme.lua")
+local ok, local_theme = pcall(dofile, wezterm.home_dir .. "/.pennykit/configs/wezterm/_local_theme.lua")
 config.color_scheme = ok and local_theme.linux or "GruvboxDark"
 
 return config
